90892e0e906d41a508bb9f1d75bf2a4c26ee6918
Natnael Mekonnen <naty@nany.zte.com.cn>
2023-03-21 10:09:29 -0400

    input validation on sign up and sign in

--- server/validate/validate.js
+++ server/validate/validate.js
@@ -1,7 +1,46 @@
-const validater=require('express-validator')
-const emailValidator=require('deep-email-validator')
-emailValidator.validate(req.body.email).then((data)=>{
-    console.log(data)
+const {check,validationResult}=require('express-validator')
+exports.validateRequest=[
+    check('firstName')
+    .notEmpty()
+    .withMessage('first name is required'),
+    check('lastName')
+    .notEmpty()
+    .withMessage('last name required'),
+    check('email')
+    .isEmail()
+    .withMessage('in valid email'),
+    check('email')
+    .notEmpty()
+    .withMessage('email required'),
+    check('password')
+    .isLength({min:6})
+    .withMessage('password must be graeter  than 6')
+
+    ]
+    exports.validatesigninRequest=[
+        check('email')
+        .isEmail()
+        .withMessage('enter valid email'),
+        check('email')
+        .notEmpty()
+        .withMessage('enter your email first'),
+        check('password')
+        .notEmpty()
+        .withMessage('enter your password to signin')
+        
+        
+        
+    ]
+    exports.isValidatedRequest=(req,res,next)=>{
+        const errors=validationResult(req)
     
-})
+        if(errors.array().length>0){
+            return res.status(400).json({
+                error:errors.array()[0].msg
+            })
+            
+        }
+
+     next()
+    }
 
